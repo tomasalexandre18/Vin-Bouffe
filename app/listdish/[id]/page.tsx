aadc87c4bbb13ea1eb2f1e49366d4519582d407f
lenadg182 <lena.degermain@campus.lasallebourges.com>
2026-06-25 11:38:59 +0200

    Update page.tsx
    les recommandation de plat sont afficher

--- app/listdish/[id]/page.tsx
+++ app/listdish/[id]/page.tsx
@@ -1,4 +1,5 @@
 import prisma from "@/libs/db";
+import TabBar from "../../components/ui/TabBar";
 
 export default async function ListDishPage({
   params,
@@ -7,11 +8,22 @@ export default async function ListDishPage({
 }) {
   const { id } = await params;
 
+  //recommandation
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  const res = await fetch(
+    `${baseUrl}/api/vin/recommendation?wine_id=${id}`,
+    { cache: "no-store" }
+  );
+  const meals = await res.json();
+
+
   const wine = await prisma.wines.findUnique({
     where: {
       wine_id: Number(id),
     },
   });
+
+
 
   if (!wine) {
     return <h1>Wine not found</h1>;
@@ -19,8 +31,19 @@ export default async function ListDishPage({
 
   return (
     <div>
+      <TabBar />
+      <div>
       <h1>{wine.wine_name}</h1>
       <p>{wine.type}</p>
+      </div>
+
+      <div>
+        <h1>Recommandation</h1>
+        <ul>
+          {meals.map((meal: any) => (<li key={meal.idMeal}> {meal.strMeal}</li>))}
+        </ul>
+      </div>
+
     </div>
   );
 }
