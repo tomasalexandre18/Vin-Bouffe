9221bc9d4d56b899cbb8bddc8591fe7f9fc1f49d
TOMAS Alexandre <alexandre.tomas@estiam.com>
2026-06-25 10:13:27 +0200

    add back url

--- app/components/ui/TabBar.tsx
+++ app/components/ui/TabBar.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 
-export default function TopBar() {
+export default function TopBar({ backUrl }: { backUrl?: string }) {
     return (
         <div className="bg-bordeaux w-screen p-3 m-0 top-0 left-0 flex flex-row justify-between items-center rounded-b-[0.5rem]">
             {/* go back */}
-            <Image src="/arrow-left.svg" alt="Back" width={24} height={24} />
+            <a href={backUrl ?? "#"} className={backUrl ? "" : "pointer-events-none"}>
+                <Image src="/arrow-left.svg" alt="Back" width={24} height={24} className={backUrl ? "cursor-pointer" : "invisible"} />
+            </a>
 
             <Image src="/winecore.svg" alt="WineCore" width={133} height={25} />
 
